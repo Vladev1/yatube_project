42e58556523f53777f72eed06d24b5f70325fad2
Владимир Мытарев <Vlaev17@yandex.ru>
2022-02-14 17:01:06 +0300

    Were added urls and views-functions

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,4 +1,4 @@
-"""yatube URL Configuration
+"""posts URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('posts.urls')),
-    path('admin/', admin.site.urls)
+    path('', views.index),
+    # Главная стрница
+    path('group/<slug:pk>/', views.group_posts)
+    # Информация о группах постов
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
+from turtle import ht
+from django import http
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request, pk):
+    return HttpResponse(f'Группа номер {pk}')
+
 
 # Create your views here.
